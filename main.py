36830dccc6688311cdae65ad4e0205980e7ce87d
Daniel Shin <kyuseung1016@gmail.com>
2024-02-05 01:14:41 +0900

    .

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from client import Client
 
 def run():
+    print(sys.argv[1], sys.argv[2])
     client = Client(sys.argv[1], sys.argv[2])
     client.smtp.sendmail('kyuds.bot@gmail.com',
               'kyuseung1016@gmail.com',
