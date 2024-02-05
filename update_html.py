411d42fd968582b19ef7000c6706f9d8ffb0e0b3
Daniel Shin <kyuseung1016@gmail.com>
2024-02-05 09:39:40 +0900

    .

---
+++ update_html.py
@@ -0,0 +1,19 @@
+import datetime
+
+HTML = """\
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url='{url}'" />
+  </head>
+  <body>
+    <a href="{url}">Click if not redirected</a>
+  </body>
+</html>
+"""
+
+if __name__ == "__main__":
+    curr = datetime.datetime.now()
+    path = "/pdf/" + curr.strftime("%Y%m%d") + ".pdf"
+    with open("index.html", "w") as file:
+        file.write(HTML.format(url=path))
