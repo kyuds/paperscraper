351be9a2beb27f8ba7937568cbe20c6bfb4ff31d
Daniel Shin <kyuseung1016@gmail.com>
2024-02-08 10:21:51 +0900

    update html style

--- update_html.py
+++ update_html.py
@@ -4,10 +4,42 @@ HTML = """\
 <!DOCTYPE html>
 <html>
   <head>
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Amiko', sans-serif;
+        letter-spacing: 8px;
+      }
+      html, body {
+          width: 100vw;
+          background-color: #030a15;
+      }
+      a {
+          color: white;
+          text-decoration: none;
+          font-size: 1rem;
+          padding: 2rem;
+          border: solid white;
+          text-align: center;
+      }
+      div {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          min-height: 100vh;
+      }
+    </style>
+    <link href="https://fonts.googleapis.com/css2?family=Amiko&display=swap" rel="stylesheet">
     <meta http-equiv="refresh" content="0; url='{url}'" />
   </head>
   <body>
-    <a href="{url}">Click if not redirected</a>
+    <div>
+      <a href="{url}">
+        <span>click here</span>
+      </a>
+    </div>
   </body>
 </html>
 """
